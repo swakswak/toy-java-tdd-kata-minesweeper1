748621c828d07426a897a0611b17c4612352f616
swakswak <hschoi0702@gmail.com>
2022-05-15 12:04:57 +0900

    지뢰 초기화 테스트 코드 추가, Cell 숫자 초기화 테스트 코드 추가

--- src/main/java/swak/swak/mineswepper/Cell.java
+++ src/main/java/swak/swak/mineswepper/Cell.java
@@ -7,11 +7,30 @@ public class Cell {
 
     private final boolean bomb;
 
-    public Cell(boolean bomb) {
+    private final Integer number;
+
+    private Cell(boolean bomb, Integer number) {
         this.bomb = bomb;
+        this.number = number;
+    }
+
+    public Cell(boolean bomb) {
+        this(bomb, null);
+    }
+
+    public Cell() {
+        this(false, null);
+    }
+
+    public Cell(Integer number) {
+        this(false, number);
     }
 
     public boolean isBomb() {
         return bomb;
+    }
+
+    public Integer getNumber() {
+        return number;
     }
 }
--- src/test/java/swak/swak/CellTest.java
+++ src/test/java/swak/swak/CellTest.java
@@ -3,22 +3,45 @@ package swak.swak;
 import org.junit.jupiter.api.Test;
 import swak.swak.mineswepper.Cell;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author hyoseok choi (hschoi0702@gmail.com)
  **/
 class CellTest {
     @Test
-    void should_BombIsTrue_InitializedInstance() {
+    void should_Bomb_True() {
         // arrange
         Cell cell = new Cell(true);
 
         // act
         boolean isBomb = cell.isBomb();
 
-        //assert
+        // assert
         assertTrue(isBomb);
+    }
+
+    @Test
+    void should_Bomb_False() {
+        // arrange
+        Cell cell = new Cell();
+
+        // act
+        boolean isBomb = cell.isBomb();
+
+        // asset
+        assertFalse(isBomb);
+    }
+
+    @Test
+    void should_Near_Number_1() {
+        // arrange
+        Cell cell = new Cell(1);
+
+        // act
+        Integer result = cell.getNumber();
+
+        // asset
+        assertEquals(1, result);
     }
 }
